6e9f3c96b9c0218ed7c3fd587c2ad37fb8af1fe6
fcoverdugoa <fcoverdugoa@underpost.net>
2021-04-13 16:01:05 -0400

    Add sha256 function

--- vanilla.js
+++ vanilla.js
@@ -517,3 +517,20 @@ function allNotClick(div){
 	notclick(el, 1, false);
 	notclick(el, 2, false);
 }
+
+async function sha256(message) {
+		/* cors restriction
+    encode as UTF-8 */
+    const msgBuffer = new TextEncoder().encode(message);
+
+    /* hash the message */
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+
+    /* convert ArrayBuffer to Array */
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+    /* convert bytes to hex string */
+    const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
+    return hashHex;
+		/* let result = await sha256('test'); */
+}
