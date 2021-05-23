e20a55e4a373662b62ea2dabdc148fe19f646758
fcoverdugoa <fcoverdugoa@underpost.net>
2021-05-23 12:59:44 -0400

    add rc function

--- underpost.net.js
+++ underpost.net.js
@@ -6,7 +6,8 @@
   var: {
     lang: lang()=='en' ? 0 : 1,
     dir: 'ltr',
-    w: null
+    w: null,
+    h: null
   },
   init: ()=>{
     s('html').lang = lang();
@@ -22,11 +23,40 @@
       `+[`Hello <a href='' >World</a>`,`Hola <a href='' >Mundo</a>`][un.var.lang]+`
 
       `);
+    },
+    rc: function(obj){
+
+      /*
+
+      un.render.rc({
+        class: '',
+        w: '',
+        h: '',
+        t: '',
+        l: '',
+        minw: '',
+        maxw: '',
+        minh: '',
+        maxw: '',
+      });
+
+      */
+
+      s(obj.class).style.width = obj.w;
+      s(obj.class).style.height = obj.h;
+      s(obj.class).style.top = obj.t;
+      s(obj.class).style.left =  obj.l;
+      s(obj.class).style.minWidth = obj.minw;
+      s(obj.class).style.maxWidth = obj.maxw;
+      s(obj.class).style.minHeight = obj.minh;
+      s(obj.class).style.maxHeight = obj.maxh;
+
     }
   },
   rr: async ()=>{
     if(un.var.w!=window.innerWidth){
       un.var.w=window.innerWidth;
+      un.var.h=window.innerHeight;
       if(un.var.w>un.param.movil){
         console.log('render -> resize desktop -> '+un.var.w);
       }else{
