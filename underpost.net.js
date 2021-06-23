2cf807c99319b4e25e20f9df9e0133a995627347
fcoverdugoa <fcoverdugoa@underpost.net>
2021-06-23 14:02:26 -0400

    update underpost.net

--- underpost.net.js
+++ underpost.net.js
@@ -1,71 +1,62 @@
-((()=>{ un = {
-  param: {
-    callback: 1000,
-    movil: 500
-  },
-  var: {
-    lang: lang()=='en' ? 0 : 1,
-    dir: 'ltr',
-    w: null,
-    h: null
-  },
-  init: ()=>{
-    s('html').lang = lang();
-    s('html').dir = un.var.dir;
-    console.log('init template system lang -> '+['en','es'][un.var.lang]);
-    un.render.init();
-    un.rr();
-  },
-  render: {
-    init: ()=>{
-      append('body', `
+((()=>{
 
-      `+[`Hello <a href='' >World</a>`,`Hola <a href='' >Mundo</a>`][un.var.lang]+`
+  //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
 
-      `);
-    },
-    rc: function(obj){
+  const render = () =>{
+    append('body', `
 
-      /*
+    `+[`Hello <a href='' >World</a>`,`Hola <a href='' >Mundo</a>`][data.const.lang]+`
 
-      un.render.rc({
-        class: '',
-        w: '',
-        h: '',
-        t: '',
-        l: '',
-        minw: '',
-        maxw: '',
-        minh: '',
-        maxh: '',
-      });
+    `);
+  };
 
-      */
+  //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
 
-      s(obj.class).style.width = obj.w;
-      s(obj.class).style.height = obj.h;
-      s(obj.class).style.top = obj.t;
-      s(obj.class).style.left =  obj.l;
-      s(obj.class).style.minWidth = obj.minw;
-      s(obj.class).style.maxWidth = obj.maxw;
-      s(obj.class).style.minHeight = obj.minh;
-      s(obj.class).style.maxHeight = obj.maxh;
-
-    }
-  },
-  rr: async ()=>{
-    if(un.var.w!=window.innerWidth){
-      un.var.w=window.innerWidth;
-      un.var.h=window.innerHeight;
-      if(un.var.w>un.param.movil){
-        console.log('render -> resize desktop -> '+un.var.w);
-      }else{
-        console.log('render -> resize movil -> '+un.var.w);
-      }
+  const rr = async ()=>{
+    if(data.var.w!=window.innerWidth || data.var.h!=window.innerHeight){
+      data.var.w=window.innerWidth;
+      data.var.h=window.innerHeight;
+      console.log('-> rr | w:'+data.var.w+' h:'+data.var.h);
     }
     console.log('callback');
-    await timer(un.param.callback);
-    un.rr();
-  }
-};un.init();
+    await timer(data.const.callback);
+    rr();
+  };
+
+  //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
+
+  let data = {
+    const: {
+      callback: 1000,
+      lang: lang()=='en' ? 0 : 1,
+      dir: 'ltr'
+    },
+    var: {
+      w: null,
+      h: null
+    }
+  };
+
+  //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
+
+  const init = ()=>{
+    s('html').lang = data.const.lang;
+    s('html').dir = data.const.dir;
+    console.log('init template system lang -> '+['en','es'][data.const.lang]);
+    render();
+    rr();
+  };
+
+  //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
+
+  init();
+
+  //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
+
 })());
