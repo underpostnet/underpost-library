5438144e39f9c27eb314c6b7caec6e0a0ab791c6
fcoverdugoa <fcoverdugoa@underpost.net>
2021-06-23 14:49:51 -0400

    update underpost.net

--- underpost.net.js
+++ underpost.net.js
@@ -3,26 +3,60 @@
   //----------------------------------------------------------------------------
   //----------------------------------------------------------------------------
 
-  const render = () =>{
-    append('body', `
+  const loader = {
+    init: () =>{
+      append('body', `
 
-    `+[`Hello <a href='' >World</a>`,`Hola <a href='' >Mundo</a>`][data.const.lang]+`
 
-    `);
-  };
 
-  //----------------------------------------------------------------------------
-  //----------------------------------------------------------------------------
+            <main class='abs center'>
 
-  const rr = async ()=>{
-    if(data.var.w!=window.innerWidth || data.var.h!=window.innerHeight){
-      data.var.w=window.innerWidth;
-      data.var.h=window.innerHeight;
-      console.log('-> rr | w:'+data.var.w+' h:'+data.var.h);
+
+                  `+[`Hello <a href='' >World</a>`,`Hola <a href='' >Mundo</a>`][data.const.lang]+`
+
+
+            </main>
+
+
+
+
+      `);
+      s('main').style.border = '2px solid white';
+    },
+    render: async () =>{
+      if(data.var.h>data.var.w){
+        s('main').style.height = data.var.w+'px';
+        s('main').style.width = data.var.w+'px';
+      }else{
+        s('main').style.height = data.var.h+'px';
+        s('main').style.width = data.var.h+'px';
+      }
     }
-    console.log('callback');
-    await timer(data.const.callback);
-    rr();
+  }
+
+  //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
+
+  const main = {
+    init: ()=> {
+      s('html').lang = ['en','es'][data.const.lang];
+      s('html').dir = data.const.dir;
+      s('body').style.overflow = 'hidden';
+      console.log('init template system lang -> '+['en','es'][data.const.lang]);
+      loader.init();
+      main.render();
+    },
+    render: async ()=>{
+      if(data.var.w!=window.innerWidth || data.var.h!=window.innerHeight){
+        data.var.w=window.innerWidth;
+        data.var.h=window.innerHeight;
+        console.log('-> rr | w:'+data.var.w+' h:'+data.var.h);
+        loader.render();
+      }
+      console.log('callback');
+      await timer(data.const.callback);
+      main.render();
+    }
   };
 
   //----------------------------------------------------------------------------
@@ -43,20 +77,6 @@
   //----------------------------------------------------------------------------
   //----------------------------------------------------------------------------
 
-  const init = ()=>{
-    s('html').lang = data.const.lang;
-    s('html').dir = data.const.dir;
-    console.log('init template system lang -> '+['en','es'][data.const.lang]);
-    render();
-    rr();
-  };
-
-  //----------------------------------------------------------------------------
-  //----------------------------------------------------------------------------
-
-  init();
-
-  //----------------------------------------------------------------------------
-  //----------------------------------------------------------------------------
+  main.init();
 
 })());
