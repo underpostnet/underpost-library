0c2029e30f47953b5c88e1121dd7d5ac1abae76d
fcoverdugoa <fcoverdugoa@underpost.net>
2021-06-23 22:21:12 -0400

    update

--- underpost.net.js
+++ underpost.net.js
@@ -1,70 +1,81 @@
 ((()=>{
 
   //----------------------------------------------------------------------------
+  // MODULES
   //----------------------------------------------------------------------------
 
-  const loader = {
-    init: () =>{
-      append('body', `
+  const base = {
+    loader: async () =>{
+      base.init.renderCss();
+      base.init.renderHtml();
+      base.init.onEvent();
+    },
+    init: {
+      renderHtml: async ()=>{
+        append('body', `
+              <main class='abs center'>
+                    <div class='abs center txt'>
+                          `+[`Hello <a href='' >World</a>`,`Hola <a href='' >Mundo</a>`][data.const.lang]+`
+                    </div>
+              </main>
+        `);
+        s('main').style.border = '2px solid white';
+      },
+      renderCss: async ()=>{
 
-
-
-            <main class='abs center'>
-
-
-                  `+[`Hello <a href='' >World</a>`,`Hola <a href='' >Mundo</a>`][data.const.lang]+`
-
-
-            </main>
-
-
-
-
-      `);
-      s('main').style.border = '2px solid white';
+        let style_txt = `
+        <style>
+          .txt:hover {
+            color: red;
+          }
+        </style>
+        `;
+        append('body', style_txt);
+      },
+      onEvent: async ()=>{
+        s('main').onclick = ()=>{
+          alert(base.service.getMainContent());
+        }
+      }
+    },
+    service: {
+      getMainContent: ()=>{
+        return s('main').innerHTML
+      }
     },
     render: async () =>{
+      s('body').style.width = data.var.w+'px';
+      s('body').style.height = data.var.h+'px';
       if(data.var.h>data.var.w){
-        s('main').style.height = data.var.w+'px';
-        s('main').style.width = data.var.w+'px';
+        s('main').style.height = data.var.w*0.95+'px';
+        s('main').style.width = data.var.w*0.95+'px';
       }else{
-        s('main').style.height = data.var.h+'px';
-        s('main').style.width = data.var.h+'px';
+        s('main').style.height = data.var.h*0.95+'px';
+        s('main').style.width = data.var.h*0.95+'px';
       }
     }
   }
 
   //----------------------------------------------------------------------------
+  // GLOBAL
   //----------------------------------------------------------------------------
 
-  const main = {
-    init: ()=> {
-      s('html').lang = ['en','es'][data.const.lang];
-      s('html').dir = data.const.dir;
-      s('body').style.overflow = 'hidden';
-      console.log('init template system lang -> '+['en','es'][data.const.lang]);
-      loader.init();
-      main.render();
+  const global = {
+    init: async ()=>{
+      base.loader();
     },
     render: async ()=>{
-      if(data.var.w!=window.innerWidth || data.var.h!=window.innerHeight){
-        data.var.w=window.innerWidth;
-        data.var.h=window.innerHeight;
-        console.log('-> rr | w:'+data.var.w+' h:'+data.var.h);
-        loader.render();
-      }
-      console.log('callback');
-      await timer(data.const.callback);
-      main.render();
+      base.render();
     }
   };
 
   //----------------------------------------------------------------------------
+  // DATA
   //----------------------------------------------------------------------------
 
   let data = {
     const: {
-      callback: 1000,
+      callback: 100,
       lang: lang()=='en' ? 0 : 1,
       dir: 'ltr'
     },
@@ -75,8 +86,32 @@
   };
 
   //----------------------------------------------------------------------------
+  // MAIN
   //----------------------------------------------------------------------------
 
+  const main = {
+    init: async ()=> {
+      s('html').lang = ['en','es'][data.const.lang];
+      s('html').dir = data.const.dir;
+      console.log('init template system lang -> '+['en','es'][data.const.lang]);
+      global.init();
+      main.render();
+    },
+    render: async ()=>{
+      if(data.var.w!=window.innerWidth || data.var.h!=window.innerHeight){
+        data.var.w=window.innerWidth;
+        data.var.h=window.innerHeight;
+        console.log('-> render | w:'+data.var.w+' h:'+data.var.h);
+        global.render();
+      }
+      console.log('callback');
+      await timer(data.const.callback);
+      main.render();
+    }
+  };
   main.init();
+
+  //----------------------------------------------------------------------------
+  //----------------------------------------------------------------------------
 
 })());
