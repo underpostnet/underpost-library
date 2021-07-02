922cb3840daab240eb9df960ab0125ec8c7b3c2c
fcoverdugoa <fcoverdugoa@underpost.net>
2021-07-02 10:29:13 -0400

    update

--- util.js
+++ util.js
@@ -119,6 +119,25 @@ if (!fs.existsSync(dir+'modules')){
 
 }
 
+
+function resolveAfter2Seconds() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000);
+  });
+}
+
+async function asyncCall() {
+  console.log('calling');
+  const result = await resolveAfter2Seconds();
+  console.log(result);
+  // expected output: "resolved"
+}
+
+asyncCall();
+
+
 */
 
 function aprox(num, dec){
