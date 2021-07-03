96db41c835660d52f2b947f72c1b7afb44469c9d
fcoverdugoa <fcoverdugoa@underpost.net>
2021-07-03 09:09:21 -0400

    update

--- util.js
+++ util.js
@@ -138,6 +138,45 @@ async function asyncCall() {
 asyncCall();
 
 
+
+
+
+
+var obj = {
+    'Students': [{
+            "name": "Raj",
+             "Age":"15",
+            "RollNumber": "123",
+            "Marks": "99",
+
+        }, {
+            "name": "Aman",
+             "Age":"14",
+            "RollNumber": "223",
+            "Marks": "69",
+           },
+           {
+            "name": "Vivek",
+             "Age":"13",
+            "RollNumber": "253",
+            "Marks": "89",
+           },
+        ]
+};
+
+var newArray = obj.Students.filter(function (el)
+{
+  return el.Age >=15 &&
+         el.RollNumber <= 200 &&
+         el.Marks >= 80 ;
+}
+);
+console.log(newArray);
+
+
+
+
+
 */
 
 function aprox(num, dec){
