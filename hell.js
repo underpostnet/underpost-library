7dd0e23cb900b62983bf6060e0fb4e6f80bf2f63
fcoverdugoa <fcoverdugoa@underpost.net>
2021-07-27 14:24:59 -0400

    Add Hell

---
+++ hell.js
@@ -0,0 +1,15 @@
+true==false ?
+        ((()=>{
+
+          console.log("true");
+          console.log("true");
+          console.log("true");
+
+         })()):
+        ((()=>{
+
+          console.log("false");
+          console.log("false");
+          console.log("false");
+
+         })());
