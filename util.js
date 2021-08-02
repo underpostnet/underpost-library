11cec0cdc2b071777efa94920aacfaa92b8a3e20
fcoverdugoa <fcoverdugoa@underpost.net>
2021-08-02 10:18:02 -0400

    add checkrut

--- util.js
+++ util.js
@@ -620,3 +620,52 @@ function pad(num, size) {
 	while (num.length < size) num = "0" + num;
 	return num;
 };
+
+
+function checkRut(rut) {
+    // Despejar Puntos
+    let valor = rut.replace('.','');
+    // Despejar Guión
+    valor = valor.replace('-','');
+
+    // Aislar Cuerpo y Dígito Verificador
+    let cuerpo = valor.slice(0,-1);
+    let dv = valor.slice(-1).toUpperCase();
+
+    // Formatear RUN
+    // rut.value = cuerpo + '-'+ dv
+
+    // Si no cumple con el mínimo ej. (n.nnn.nnn)
+    if(cuerpo.length < 7) { return false;}
+
+    // Calcular Dígito Verificador
+    let suma = 0;
+    let multiplo = 2;
+
+    // Para cada dígito del Cuerpo
+    for(let i=1;i<=cuerpo.length;i++) {
+
+        // Obtener su Producto con el Múltiplo Correspondiente
+        let index = multiplo * valor.charAt(cuerpo.length - i);
+
+        // Sumar al Contador General
+        suma = suma + index;
+
+        // Consolidar Múltiplo dentro del rango [2,7]
+        if(multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }
+
+    }
+
+    // Calcular Dígito Verificador en base al Módulo 11
+    let dvEsperado = 11 - (suma % 11);
+
+    // Casos Especiales (0 y K)
+    dv = (dv == 'K')?10:dv;
+    dv = (dv == 0)?11:dv;
+
+    // Validar que el Cuerpo coincide con su Dígito Verificador
+    if(dvEsperado != dv) { return false; }
+
+    // Si todo sale bien, eliminar errores (decretar que es válido)
+    return true;
+  }
