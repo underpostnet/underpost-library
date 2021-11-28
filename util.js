7b855406859334006f39c425d59611c71c65bfd8
fcoverdugoa <fcoverdugoa@underpost.net>
2021-11-27 23:40:17 -0300

    update

--- util.js
+++ util.js
@@ -1118,10 +1118,15 @@ function makeid(length, num) {
 function getRandomColor() {
   let letters = '0123456789ABCDEF';
   let color = '#';
-  for (var i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
+}
+
+
+function jsonWebRender(objRender){
+	return `JSON.parse(`+'`'+JSONstr(objRender)+'`'+`);`;
 }
 
 
