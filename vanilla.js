d3bf5f5e1f9db1e55b0f51d1d8a11589f6c15ba7
fcoverdugo <fcoverdugoa@underpost.net>
2022-06-14 22:28:37 -0400

    update

--- vanilla.js
+++ vanilla.js
@@ -2476,6 +2476,33 @@ function setURI(_uri, _objData, _title){
 	return history.pushState(_objData, _title, _uri);
 }
 
+class Rest {
+	async FETCH(url, method, data, options) {
+	  const defaultOptions = {
+		mode: 'cors',
+		cache: 'no-cache',
+		credentials: 'same-origin',
+		headers: {
+		  'Content-Type': 'application/json'
+		  // 'navigator-onLine': navigator.onLine
+		},
+		redirect: 'follow',
+		referrerPolicy: 'no-referrer'
+	  };
+	  try {
+		const response = await fetch(url, {
+		  method: method,
+		  ...(options == undefined ? defaultOptions : options),
+		  body: data == undefined ? undefined : JSON.stringify(data)
+		});
+		return response.json();
+	  }catch(error){
+		console.error(error);
+		return {success: false, error }
+	  }
+	}
+  }
+
 /*
 
 // receiver stat to changue view
